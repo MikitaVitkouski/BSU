9d0540c6164f53b5138749b4aec37183925d9312
MikitaVitkouski <sovenokrelax@gmail.com>
2023-12-05 21:48:32 +0300

    Have done the third task of Control Work 05/12/2023, wrote unit-tests for class Buffer

---
+++ Java/Control_Work_05_12_2023/Task3/MainApp.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+
+public class MainApp {
+    public static void main(String[] args) {
+        Buffer buffer = new Buffer();
+
+        Thread producerThread = new Thread(new Producer(buffer));
+        Thread consumerThread = new Thread(new Consumer(buffer));
+
+        producerThread.start();
+        consumerThread.start();
+    }
+}
