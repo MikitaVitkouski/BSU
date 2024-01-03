0c93f6cde8ffec81726e81901f0e16522deac0b3
blowN_Y <sovenokrelax@gmail.com>
2024-01-03 18:29:05 +0300

    Fixed syntax error in unit-tests for Main.cpp

--- Operating-Systems/LW2_Creating_Threads/Unit-tests/MainTests (BoostTest).cpp
+++ Operating-Systems/LW2_Creating_Threads/Unit-tests/MainTests (BoostTest).cpp
@@ -19,7 +19,10 @@ BOOST_AUTO_TEST_CASE(FindMinMaxThreadTest) {
     HANDLE hThread = CreateThread(NULL, 0, FindMinMaxThread, &arr, 0, NULL);
     WaitForSingleObject(hThread, INFINITE);
 
-    int result = GetExitCodeThread(hThread, &result) ? result : -1;
+    DWORD result;
+    int success = GetExitCodeThread(hThread, &result);
+
+    int exitCode = (success != 0) ? result : -1;
     int min = result & 0xFFFF;
     int max = result >> 16;
 
@@ -30,12 +33,16 @@ BOOST_AUTO_TEST_CASE(FindMinMaxThreadTest) {
 }
 
 BOOST_AUTO_TEST_CASE(AverageThreadTest) {
-    std::vector<int> arr = { 5, 2, 8, 1, 9, 3 };
+    std::vector<int> arr = { 1,2,3,4,5 };
 
     HANDLE hThread = CreateThread(NULL, 0, AverageThread, &arr, 0, NULL);
+    if (hThread == NULL) {
+        // error
+    }
+
     WaitForSingleObject(hThread, INFINITE);
 
-    BOOST_CHECK_EQUAL_COLLECTIONS(arr.begin(), arr.end(), std::vector<int>({ 5, 3, 8, 4, 5, 3 }).begin(), std::vector<int>({ 5, 3, 8, 4, 5, 3 }).end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(arr.begin(), arr.end(), std::vector<int>({ 3,2,3,4,3 }).begin(), std::vector<int>({ 3,2,3,4,3 }).end());
 
     CloseHandle(hThread);
 }
