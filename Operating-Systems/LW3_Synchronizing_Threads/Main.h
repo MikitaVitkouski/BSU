49b26c52d3e338ff21e8043fa6ade9132ae3c434
blowN_Y <sovenokrelax@gmail.com>
2024-01-03 19:15:52 +0300

    Ok, reorganized project LW3, cuz unit-tests hadn't worked properly, there was conflict during compilation via Test Explorer, honestly i don't know is it right, cuz now I have class implementation in header file... holy crap :(

--- Operating-Systems/LW3_Synchronizing_Threads/Main.h
+++ Operating-Systems/LW3_Synchronizing_Threads/Main.h
@@ -13,9 +13,19 @@ public:
     MarkerThread(int markerNumber, std::vector<int>& array, int arraySize, std::mutex& arrayMutex, std::condition_variable& cv, std::atomic<bool>& startSignal, std::atomic<bool>& continueSignal)
         : markerNumber(markerNumber), array(array), arraySize(arraySize), arrayMutex(arrayMutex), cv(cv), startSignal(startSignal), continueSignal(continueSignal), isTerminated(false) {}
 
-    void start();
+    void start() {
+        thread = std::thread(&MarkerThread::run, this);
+    }
 
-    void terminate();
+    void terminate() {
+        {
+            std::unique_lock<std::mutex> lock(arrayMutex);
+            isTerminated = true;
+        }
+        cv.notify_all();
+        thread.join();
+        std::cout << "Marker " << markerNumber << ": Thread terminated" << std::endl;
+    }
 
 private:
     int markerNumber;
@@ -28,7 +38,53 @@ private:
     std::atomic<bool>& continueSignal;
     bool isTerminated;
 
-    void run();
+    void run() {
+        std::cout << "Marker " << markerNumber << ": Thread started" << std::endl;
+
+        srand(static_cast<unsigned int>(markerNumber));
+
+        {
+            std::unique_lock<std::mutex> lock(arrayMutex);
+            cv.wait(lock, [&] { return startSignal.load(); });
+        }
+
+        while (true) {
+            int randomNumber = rand();
+            int index = randomNumber % arraySize;
+
+            {
+                std::unique_lock<std::mutex> lock(arrayMutex);
+
+                if (array[index] == 0) {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+                    array[index] = markerNumber;
+                    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+
+                    std::cout << "Marker " << markerNumber << ": Marked element at index " << index << std::endl;
+                }
+                else {
+                    std::cout << "Marker " << markerNumber << ": Element at index " << index << " is already marked" << std::endl;
+                    std::cout << "Marker " << markerNumber << ": Signaling main thread about inability to continue" << std::endl;
+                    continueSignal.store(false);
+                    cv.notify_one();
+                    cv.wait(lock, [&] { return startSignal.load() || isTerminated; });
+
+                    if (isTerminated) {
+                        break;
+                    }
+                }
+            }
+
+            std::unique_lock<std::mutex> lock(arrayMutex);
+            cv.wait(lock, [&] { return continueSignal.load() || isTerminated; });
+
+            if (isTerminated) {
+                break;
+            }
+        }
+
+        std::cout << "Marker " << markerNumber << ": Thread ended" << std::endl;
+    }
 
 public:
     bool getTerminationStatus() const {
