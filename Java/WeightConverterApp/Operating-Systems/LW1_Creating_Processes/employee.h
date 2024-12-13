c409dd60cfdef04c9507b1d7e189a595ef4e1a75
MikitaVitkouski <149664286+MikitaVitkouski@users.noreply.github.com>
2024-12-13 23:06:06 +0300

    Delete Java/WeightConverterApp/Operating-Systems directory

--- Java/WeightConverterApp/Operating-Systems/LW1_Creating_Processes/employee.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct employee
-{
-int num; //employee's ID
-char name[10]; //employee's Name
-double hours; //hours worked
-};
--- Java/WeightConverterApp/Operating-Systems/LW5_Data_Exchange_via_Named_Channels/employee.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct employee {
-    int num;
-    wchar_t name[100];
-    double hours;
-};
