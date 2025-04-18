410bbae7adee8f379c99d63b44944c9ed63bac22
MikitaVitkouski <sovenokrelax@gmail.com>
2025-04-18 10:06:34 +0300

    Added display() method to show the info about students, wrote the example with the use of smart pointers and STL algorithms

--- C++/Learning_CPP/ModernCppPlayground.cpp
+++ C++/Learning_CPP/ModernCppPlayground.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <memory>
+#include <string>
 
 using namespace std;
 
@@ -16,11 +18,33 @@ public:
 	double average() const {
 		return accumulate(grades.begin(), grades.end(), 0.0) / grades.size();
 	}
+
+	void display() const {
+		cout << name << " - average grade: " << average() << endl;
+	}
+
+	const string& getName() const { return name;  }
 };
 
 int main() {
+	vector<unique_ptr<Student>> students;
+	students.push_back(make_unique<Student>("Nikita", vector<int>{90, 85, 92}));
+	students.push_back(make_unique <Student>("Julia", vector<int>{70, 60, 75}));
+	students.push_back(make_unique<Student>("Timofey", vector<int>{88, 90, 85}));
 
+	cout << "\nStudent and their average grades:\n";
+	for (const auto& student : students) {
+		student->display();
+	}
 
+	sort(students.begin(), students.end(), [](const auto& a, const auto& b) {
+		return a->average() > b->average();
+		});
+
+	cout << "\nSorted by average grades:\n";
+	for (const auto& student : students) {
+		student->display();
+	}
 
 	return 0;
 }
