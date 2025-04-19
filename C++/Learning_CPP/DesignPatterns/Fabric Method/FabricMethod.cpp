39afe67b2c7cfb35f3f578b044c73de0555542e2
MikitaVitkouski <sovenokrelax@gmail.com>
2025-04-19 18:43:49 +0300

    Implemented Notification creators: EmailNotificationCreator, SMSNotificationCreator, subclass for them is NotificationCreator

--- C++/Learning_CPP/DesignPatterns/Fabric Method/FabricMethod.cpp
+++ C++/Learning_CPP/DesignPatterns/Fabric Method/FabricMethod.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <memory>
+#include <string>
 
 class Notification {
 public:
@@ -17,6 +19,32 @@ class SMSNotification : public Notification {
 public:
 	void send(const std::string& message) {
 		std::cout << "Sending SMS: " << message << std::endl;
+	}
+};
+
+class NotificationCreator {
+public:
+	virtual std::unique_ptr<Notification> createNotification() const = 0;
+
+	void notify(const std::string& message) const {
+		auto notification = createNotification();
+		notification->send(message);
+	}
+
+	virtual ~NotificationCreator() = default;
+};
+
+class EmailNotificationCreator : public NotificationCreator {
+public:
+	std::unique_ptr<Notification> createNotification() const override {
+		return std::make_unique<EmailNotification>();
+	}
+};
+
+class SMSNoficationCenter : public NotificationCreator {
+public:
+	std::unique_ptr<Notification> createNotification() const override {
+		return std::make_unique<EmailNotification>();
 	}
 };
 
