0da1c013fa0da84644930fb019411252e966bdb2
MikitaVitkouski <sovenokrelax@gmail.com>
2025-04-20 12:47:48 +0300

    Implemented concrete prototypes - Circle and Rectangle classes

--- C++/Learning_CPP/DesignPatterns/Prototype/Prototype.cpp
+++ C++/Learning_CPP/DesignPatterns/Prototype/Prototype.cpp
@@ -22,6 +22,50 @@ public:
 	virtual ~Shape() = default;
 };
 
+class Circle : public Shape {
+private:
+	int radius;
+public:
+	Circle(int r = 0) : radius(r) { }
+
+	Circle(const Circle& other) : Shape(other), radius(other.radius) { }
+
+	std::unique_ptr<Shape> clone() const override {
+		return std::make_unique<Circle>(*this);
+	}
+
+	void draw() const override {
+		std::cout << "Circle with radius " << radius << " and color " << color << "\n";
+	}
+
+	void setRadius(int r) {
+		radius = r;
+	}
+};
+
+class Rectangle : public Shape {
+private:
+	int width;
+	int height;
+public:
+	Rectangle(int w = 0, int h = 0) : width(w), height(h) { }
+
+	Rectangle(const Rectangle& other) : Shape(other), width(other.width), height(other.height) { }
+
+	std::unique_ptr<Shape> clone() const override {
+		return std::make_unique<Circle>(*this);
+	}
+
+	void draw() const override {
+		std::cout << "Rectangle " << width << "x" << height << " and color " << color << "\n";
+	}
+
+	void setSize(int w, int h) {
+		width = w;
+		height = h;
+	}
+};
+
 int main() {
 
 
