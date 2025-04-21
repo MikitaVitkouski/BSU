38620670da0231de8e02c956f4b6cf9e44bbc363
MikitaVitkouski <sovenokrelax@gmail.com>
2025-04-21 10:37:03 +0300

    Implemented abstract Color class, its subclasses Red and Blue and Abstract class Shape

---
+++ C++/Learning_CPP/DesignPatterns/Bridge/Bridge.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+class Color {
+public:
+	virtual std::string getColor() const = 0;
+	virtual ~Color() = default;
+};
+
+class Red : public Color {
+public:
+	std::string getColor() const override {
+		return "Red";
+	}
+};
+
+class Blue : public Color {
+public:
+	std::string getColor() const override {
+		return "Blue";
+	}
+}; 
+
+class Shape {
+protected:
+	std::shared_ptr<Color> color;
+public:
+	Shape(std::shared_ptr<Color> c) : color(std::move(c)) { }
+	virtual void draw() const = 0;
+	virtual ~Shape() = default;
+};
+
+int main() {
+
+
+	return 0;
+}
