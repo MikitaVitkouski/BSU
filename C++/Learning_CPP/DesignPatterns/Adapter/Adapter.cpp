1a3b8009ba21f18b337368091f68590ec62082ee
MikitaVitkouski <sovenokrelax@gmail.com>
2025-04-21 10:27:50 +0300

    Implemented the Adapter class making it possible for the Store class to use a third-party payment system - BankPaymentService - without modifying its own business logic

--- C++/Learning_CPP/DesignPatterns/Adapter/Adapter.cpp
+++ C++/Learning_CPP/DesignPatterns/Adapter/Adapter.cpp
@@ -15,6 +15,17 @@ public:
 	}
 };
 
+class BankPaymentAdapter : public IPaymentProcessor {
+private:
+	std::shared_ptr<BankPaymentService> service;
+public:
+	BankPaymentAdapter(std::shared_ptr<BankPaymentService> srv) : service(std::move(srv)) { }
+
+	void pay(double amount) override {
+		service->makeTransaction(amount, "USD");
+	}
+};
+
 class Store {
 private:
 	std::shared_ptr<IPaymentProcessor> paymentProcessor;
@@ -28,7 +39,11 @@ public:
 };
 
 int main() {
+	auto bankService = std::make_shared<BankPaymentService>();
+	auto adapter = std::make_shared<BankPaymentAdapter>(bankService);
 
+	Store myStore(adapter);
+	myStore.purchase(99.99);
 
 	return 0;
 }
