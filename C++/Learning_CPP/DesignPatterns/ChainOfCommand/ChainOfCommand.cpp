187fd6a8e4b07d19921a67ae34ecbb49af344c61
MikitaVitkouski <sovenokrelax@gmail.com>
2025-04-23 21:34:40 +0300

    Implemented BaseHandler class. If he doesn't handle issue - issue moves forward

--- C++/Learning_CPP/DesignPatterns/ChainOfCommand/ChainOfCommand.cpp
+++ C++/Learning_CPP/DesignPatterns/ChainOfCommand/ChainOfCommand.cpp
@@ -9,6 +9,25 @@ public:
 	virtual ~Handler() = default;
 };
 
+class BaseHandler : public Handler {
+protected:
+	Handler* next = nullptr;
+public:
+	Handler* setNext(Handler* nextHandler) override {
+		next = nextHandler;
+		return nextHandler;
+	}
+
+	void handleRequest(const std::string& issue) override {
+		if (next) {
+			next->handleRequest(issue);
+		}
+		else {
+			std::cout << "No one can handle this issue: " << issue << "\n";
+		}
+	}
+};
+
 int main() {
 
 
