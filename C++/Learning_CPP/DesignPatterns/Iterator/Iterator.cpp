e68a0b315b99c22aed1ce814fcd6853b9a64daa7
MikitaVitkouski <sovenokrelax@gmail.com>
2025-04-24 23:23:20 +0300

    Add Book class and IBookCollection interface for Iterator pattern

--- C++/Learning_CPP/DesignPatterns/Iterator/Iterator.cpp
+++ C++/Learning_CPP/DesignPatterns/Iterator/Iterator.cpp
@@ -10,6 +10,22 @@ public:
 	virtual T next() = 0;
 };
 
+class Book {
+private:
+	std::string title;
+public:
+	Book(const std::string& title) : title(title) { }
+	std::string getTitle() const {
+		return title;
+	}
+};
+
+class IBookCollection {
+public:
+	virtual ~IBookCollection() = default;
+	virtual std::unique_ptr<IIterator<Book>> createIterator() = 0;
+};
+
 
 
 int main() {
