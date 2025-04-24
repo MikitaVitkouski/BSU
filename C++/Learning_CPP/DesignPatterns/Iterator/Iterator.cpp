7f124b54739f743c25ed1393845cabe90ee84ab4
MikitaVitkouski <sovenokrelax@gmail.com>
2025-04-24 23:29:00 +0300

    Add concrete collection - BookCollection, and concrete iterator - BookIterator

--- C++/Learning_CPP/DesignPatterns/Iterator/Iterator.cpp
+++ C++/Learning_CPP/DesignPatterns/Iterator/Iterator.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <vector>
 
 template<typename T>
 class IIterator {
@@ -26,7 +27,36 @@ public:
 	virtual std::unique_ptr<IIterator<Book>> createIterator() = 0;
 };
 
+class BookCollection : public IBookCollection {
+private:
+	std::vector<Book> books;
+public:
+	void addBook(const Book& book) {
+		books.push_back(book);
+	}
 
+	const std::vector<Book>& getBooks() const {
+		return books;
+	}
+
+	std::unique_ptr<IIterator<Book>> createIterator() override;
+};
+
+class BookIterator : public IIterator<Book> {
+private:
+	const std::vector<Book>& books;
+	size_t position = 0;
+public:
+	BookIterator(const std::vector<Book>& books) : books(books) { }
+
+	bool hasNext() override {
+		return position < books.size();
+	}
+
+	Book next() override {
+		return books[position++];
+	}
+};
 
 int main() {
 
