6382d28e447e358f3b68d28f0eecd99af9981337
MikitaVitkouski <sovenokrelax@gmail.com>
2025-04-25 13:40:22 +0300

    Implemented different states of ATM: NoCardState, HasCardState, AuthenticatedState

--- C++/Learning_CPP/DesignPatterns/State/State.cpp
+++ C++/Learning_CPP/DesignPatterns/State/State.cpp
@@ -12,7 +12,40 @@ public:
 	virtual ~ATMState() = default;
 };
 
+class NoCardState : public ATMState {
+	void insertCard(ATM& atm) override;
+	void enterPIN(ATM& atm, int) override {
+		std::cout << "Insert card first.\n";
+	}
+	void withdrawCash(ATM& atm, int) override {
+		std::cout << "Insert card first.\n";
+	}
+};
 
+class HasCardState : public ATMState {
+public:
+	void insertCard(ATM& atm) override {
+		std::cout << "Card already inserted.\n";
+	}
+
+	void enterPIN(ATM& atm, int pin) override;
+	void withdrawCash(ATM& atm, int) override {
+		std::cout << "Enter PIN first.\n";
+	}
+};
+
+class AuthenticatedState : public ATMState {
+public:
+	void insertCard(ATM& atm) override {
+		std::cout << "Already authenticated.\n";
+	}
+
+	void enterPIN(ATM& atm, int) override {
+		std::cout << "Already entered PIN.\n";
+	}
+
+	void withdrawCash(ATM& atm, int amount) override;
+};
 
 int main() {
 
