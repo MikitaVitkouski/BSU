3bf14fd82303ecfef17d0e4ef336070d69f9da0f
MikitaVitkouski <sovenokrelax@gmail.com>
2025-04-25 12:39:00 +0300

    Implement an example of the situation: subscribers get news from news agency, news agency can remove subscribers, subscribers get notified about every new news agency have posted

--- C++/Learning_CPP/DesignPatterns/Observer/Observer.cpp
+++ C++/Learning_CPP/DesignPatterns/Observer/Observer.cpp
@@ -21,7 +21,7 @@ public:
 		subscribers.erase(std::remove(subscribers.begin(), subscribers.end(), sub), subscribers.end());
 	}
 
-	void AddNews(const std::string& news) {
+	void addNews(const std::string& news) {
 		latestNews = news;
 		notify();
 	}
@@ -39,12 +39,33 @@ public:
 	Reader(const std::string& n) : name(n) { }
 
 	void update(const std::string& news) override {
-		std::cout << name << " ???????(?) ???????: " << news << "\n";
+		std::cout << name << " received the new: " << news << "\n";
 	}
 };
 
 int main() {
+	NewsAgency agency;
 
+	Reader nikita("Nikita");
+	Reader jeka("Jeka");
+
+	agency.subscribe(&nikita);
+	agency.subscribe(&jeka);
+
+	agency.addNews("Tomorrow we'll have elections!");
+	agency.addNews("Tomorrow will be sunny");
+
+	agency.unsubscribe(&jeka);
+
+	agency.addNews("Our team has won Olympic Games!");
+
+	/*Output:
+	Nikita received the new: Tomorrow we'll have elections!
+	Jeka received the new: Tomorrow we'll have elections!
+	Nikita received the new: Tomorrow will be sunny
+	Jeka received the new: Tomorrow will be sunny
+	Nikita received the new: Our team has won Olympic Games!
+	*/
 
 	return 0;
 }
