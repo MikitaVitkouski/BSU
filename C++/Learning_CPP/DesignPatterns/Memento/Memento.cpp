cedd4f322609a82748161a64ef95b368a5c7038b
MikitaVitkouski <sovenokrelax@gmail.com>
2025-04-25 12:03:20 +0300

    Implement CareTaker - History to manage mementos, it stores stack of mementos for backups

--- C++/Learning_CPP/DesignPatterns/Memento/Memento.cpp
+++ C++/Learning_CPP/DesignPatterns/Memento/Memento.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
 #include <string>
-#include <memory>
-#include <vector>
+#include <stack>
 
+class Memento {
+	std::string state;
+public:
+	Memento(const std::string& state) : state(state) { }
+	std::string getState() const {
+		return state;
+	}
+};
 
+class TextEditor {
+	std::string text;
+public:
+	void type(const std::string& newText) {
+		text += newText;
+	}
+
+	void showText() const {
+		std::cout << "Current text: " << text << "\n";
+	}
+
+	Memento save() const {
+		return Memento(text);
+	}
+
+	void restore(const Memento& memento) {
+		text = memento.getState();
+	}
+};
+
+class History {
+	std::stack<Memento> mementos;
+public:
+	void backup(const Memento& memento) {
+		mementos.push(memento);
+	}
+
+	Memento undo() {
+		if (mementos.empty()) {
+			std::cout << "Nothing to undo.\n";
+			return Memento("");
+		}
+		Memento last = mementos.top();
+		mementos.pop();
+		return last;
+	}
+};
 
 int main() {
 
