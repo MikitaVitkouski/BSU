cc211f113aeb2d2c30ef96bb910ac2a84bd1f9e8
MikitaVitkouski <sovenokrelax@gmail.com>
2025-04-28 21:06:37 +0300

    Remove the project cuz of wrong configuration

--- C++/Projects/FractionCalculator/FractionCalculator/Fraction.cpp
+++
@@ -1,4 +0,0 @@
-#include "Fraction.h"
-
-Fraction::Fraction(int numerator, int denominator)
-    : numerator_(numerator), denominator_(denominator) {}
--- C++/Projects/FractionCalculator/FractionCalculator/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Fraction.h"
-
-int main() {
-    Fraction f(1, 2);
-    std::cout << "Fraction Calculator started!" << std::endl;
-    return 0;
-}
