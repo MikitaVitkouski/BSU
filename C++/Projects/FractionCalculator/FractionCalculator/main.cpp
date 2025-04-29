85cd55f85d13ead156c31ad0d9320bddc22be68d
MikitaVitkouski <sovenokrelax@gmail.com>
2025-04-29 21:52:23 +0300

    Implement example of client code using our calculator, results to be written to output.txt. Future modifications: more operator overload, more methods, write GTest unit-tests

--- C++/Projects/FractionCalculator/FractionCalculator/main.cpp
+++ C++/Projects/FractionCalculator/FractionCalculator/main.cpp
@@ -1,16 +1,30 @@
 #include "Fraction.h"
+#include <fstream>
+
+std::ofstream out("output.txt");
 
 int main() {
 	try {
+
 		Fraction f1(1, 2);
 		Fraction f2(3, 4);
 		Fraction f3(1, 4);
 
 		Fraction sum = f1 + f2;
-		std::cout << "f1 + f2: " << sum << "\n";
-	}
-	catch (const std::exception& e) {
-		std::cout << "Error: " << e.what() << std::endl;
+		out << "f1 + f2: " << sum << "\n";
+
+		Fraction diff = f1 - f2;
+		out << "f1 - f2 = " << diff << "\n";
+
+		Fraction mul = f1 * f3;
+		out << "f1 * f3 = " << mul << "\n";
+
+		Fraction div = f3 / f2;
+		out << "f3 / f2 = " << div << "\n";
+
+		Fraction f4(1, 0);
+	} catch (const std::exception& e) {
+		out << "Error: " << e.what() << std::endl;
 	}
 
 	return 0;
