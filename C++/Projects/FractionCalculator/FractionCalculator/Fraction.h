f7bca597c1ae58b813a76ead048a2cf6377d7a61
MikitaVitkouski <sovenokrelax@gmail.com>
2025-05-01 21:18:20 +0300

    Add function's signatures of class Fraction for comparing with int, double, decimal representation, ability of casting to double, string representation

--- C++/Projects/FractionCalculator/FractionCalculator/Fraction.h
+++ C++/Projects/FractionCalculator/FractionCalculator/Fraction.h
@@ -20,10 +20,18 @@ public:
 	Fraction operator-(const Fraction& other) const;
 	Fraction operator*(const Fraction& other) const;
 	Fraction operator/(const Fraction& other) const;
+
 	Fraction& operator+=(const Fraction& other);
 	Fraction& operator-=(const Fraction& other);
 	Fraction& operator*=(const Fraction& other);
 	Fraction& operator/=(const Fraction& other);
+
+	Fraction operator+() const; //unary plus
+	Fraction operator-() const; //unary minus
+	Fraction& operator++(); //prefix++
+	Fraction operator++(int); //postfix++
+	Fraction& operator--(); //prefix--
+	Fraction operator--(int); //postfix--
 
 	bool operator==(const Fraction& other) const;
 	bool operator>=(const Fraction& other) const;
@@ -31,6 +39,26 @@ public:
 	bool operator>(const Fraction& other) const;
 	bool operator<(const Fraction& other) const;
 	bool operator!=(const Fraction& other) const;
+
+	//comparing with int
+	bool operator==(int value) const;
+	bool operator>(int value) const;
+	bool operator<(int value) const;
+	bool operator<=(int value) const;
+	bool operator>=(int value) const;
+	bool operator!=(int value) const;
+
+	//comparing with double
+	bool operator==(double value) const;
+	bool operator>(double value) const;
+	bool operator<(double value) const;
+	bool operator<=(double value) const;
+	bool operator>=(double value) const;
+	bool operator!=(double value) const;
+
+	double toDouble() const; // returns decimal representation of the fraction
+	std::string toString() const; // for future purpose (GUI)
+	explicit operator double() const; // for "static_cast<double>(Fraction_object)"
 
 	friend std::istream& operator>>(std::istream& is, Fraction& frac);
 	friend std::ostream& operator<<(std::ostream& os, const Fraction& frac);
