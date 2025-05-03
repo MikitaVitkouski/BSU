90303618813a889b9cc0fae3dcf0483a308008ac
MikitaVitkouski <sovenokrelax@gmail.com>
2025-05-03 21:14:41 +0300

    Add unit-tests for Multiplication and Division to test operator* and operator/ overloads. Success

--- C++/Projects/FractionCalculator/FractionTests/test.cpp
+++ C++/Projects/FractionCalculator/FractionTests/test.cpp
@@ -18,3 +18,21 @@ TEST(FractionTest, Subtraction) {
     EXPECT_EQ(result.getNumerator(), 7);
     EXPECT_EQ(result.getDenominator(), 6);
 }
+
+TEST(FractionTest, Multiplication) {
+    Fraction f1(1, 4);
+    Fraction f2(5, 3);
+    Fraction result = f1 * f2;
+
+    EXPECT_EQ(result.getNumerator(), 5);
+    EXPECT_EQ(result.getDenominator(), 12);
+}
+
+TEST(FractionTest, Division) {
+    Fraction f1(1, 3);
+    Fraction f2(5, 7);
+    Fraction result = f2 / f1;
+
+    EXPECT_EQ(result.getNumerator(), 15);
+    EXPECT_EQ(result.getDenominator(), 7);
+}
