eb25f6cd143807de74d991c22237c07a794005e4
MikitaVitkouski <sovenokrelax@gmail.com>
2025-05-04 21:54:15 +0300

    Add and pass unit tests for Fraction::operator+, -, *,/, Reduction

--- C++/Projects/FractionCalculator/FractionTests/test.cpp
+++ C++/Projects/FractionCalculator/FractionTests/test.cpp
@@ -10,12 +10,48 @@ TEST(FractionTest, Addition) {
     EXPECT_EQ(result.getDenominator(), 6);
 }
 
+TEST(FractionTest, NegativeAddition) {
+    Fraction a(-1, 2);
+    Fraction b(1, 3);
+    Fraction result = a + b;
+
+    EXPECT_EQ(result.getNumerator(), -1);
+    EXPECT_EQ(result.getDenominator(), 6);
+}
+
+TEST(FractionTest, ZeroAddition) {
+    Fraction zero(0, 1);
+    Fraction b(1, 3);
+    Fraction result = zero + b;
+
+    EXPECT_EQ(result.getNumerator(), 1);
+    EXPECT_EQ(result.getDenominator(), 3);
+}
+
+TEST(FractionTest, Reduction) {
+    Fraction a(3, 6);
+    Fraction b(4, 8);
+    Fraction result = a + b;
+
+    EXPECT_EQ(result.getNumerator(), 1);
+    EXPECT_EQ(result.getDenominator(), 1);
+}
+
 TEST(FractionTest, Subtraction) {
     Fraction f1(1, 3);
     Fraction f2(3, 2);
     Fraction result = f2 - f1;
 
     EXPECT_EQ(result.getNumerator(), 7);
+    EXPECT_EQ(result.getDenominator(), 6);
+}
+
+TEST(FractionTest, NegativeSubtraction) {
+    Fraction a(-1, 2);
+    Fraction b(-1, 3);
+    Fraction result = a - b;
+
+    EXPECT_EQ(result.getNumerator(), -1);
     EXPECT_EQ(result.getDenominator(), 6);
 }
 
@@ -28,6 +64,15 @@ TEST(FractionTest, Multiplication) {
     EXPECT_EQ(result.getDenominator(), 12);
 }
 
+TEST(FractionTest, OneMultiplication) {
+    Fraction one(1, 1);
+    Fraction f(5, 3);
+    Fraction result = one * f;
+
+    EXPECT_EQ(result.getNumerator(), 5);
+    EXPECT_EQ(result.getDenominator(), 3);
+}
+
 TEST(FractionTest, Division) {
     Fraction f1(1, 3);
     Fraction f2(5, 7);
@@ -35,4 +80,24 @@ TEST(FractionTest, Division) {
 
     EXPECT_EQ(result.getNumerator(), 15);
     EXPECT_EQ(result.getDenominator(), 7);
+}
+
+TEST(FractionTest, DivisionByZeroNumerator) {
+    Fraction f1(1, 5);
+    Fraction f2(0, 7);
+
+    EXPECT_THROW(f1 / f2, std::invalid_argument);
+}
+
+TEST(FractionTest, ZeroDividedByFraction) {
+    Fraction zero(0, 8);
+    Fraction f(5, 7);
+    Fraction result = zero / f;
+
+    EXPECT_EQ(result.getNumerator(), 0);
+    EXPECT_EQ(result.getDenominator(), 1);
+}
+
+TEST(FractionTest, InvalidDenominator) {
+    EXPECT_THROW(Fraction(1, 0), std::invalid_argument);
 }
