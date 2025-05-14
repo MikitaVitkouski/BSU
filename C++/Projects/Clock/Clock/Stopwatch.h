4b436653b303a551dc23616e66f906a9d5b93a25
MikitaVitkouski <sovenokrelax@gmail.com>
2025-05-14 21:00:12 +0300

    Add method's signatures for Stopwatch in .h file, add data's fields

--- C++/Projects/Clock/Clock/Stopwatch.h
+++ C++/Projects/Clock/Clock/Stopwatch.h
@@ -1,15 +1,27 @@
 #ifndef STOPWATCH_H
 #define STOPWATCH_H
 
-#include <iostream>
+#include <chrono>
+#include <vector>
 
 class Stopwatch {
+private:
+	bool running = false;
+	std::chrono::steady_clock::time_point startTime;
+	std::chrono::milliseconds accumulatedTime{0};
+	std::vector<std::chrono::milliseconds> laps;
 public:
 	Stopwatch();
 
 	void start();
 	void stop();
 	void reset();
+	void lap();
+
+	bool isRunning() const;
+	std::chrono::milliseconds elapsed() const;
+	const std::vector <std::chrono::milliseconds>& getLaps() const;
+
 };
 
 #endif
