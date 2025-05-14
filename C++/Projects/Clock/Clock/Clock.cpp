56e01961a3ea40d7996ad63ba38debcf08c5abe5
MikitaVitkouski <sovenokrelax@gmail.com>
2025-05-14 21:32:20 +0300

    Implement class Clock

--- C++/Projects/Clock/Clock/Clock.cpp
+++ C++/Projects/Clock/Clock/Clock.cpp
@@ -1,2 +1,18 @@
 #include "Clock.h"
+#include <chrono>
+#include <iomanip>
+#include <sstream>
 
+Clock::Clock() {
+
+}
+
+std::string Clock::getTime() const {
+	auto now = std::chrono::system_clock::now();
+	std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+	std::tm localTime = *std::localtime(&currentTime);
+
+	std::ostringstream oss;
+	oss << std::put_time(&localTime, "%H:%M:%S");
+	return oss.str();
+}
