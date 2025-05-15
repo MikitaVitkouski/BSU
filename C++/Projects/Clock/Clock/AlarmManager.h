967587026107a6047ca55e5f0d8daf598ca29e28
MikitaVitkouski <sovenokrelax@gmail.com>
2025-05-15 22:49:45 +0300

    Add TimerManager signatures and class Alarm in TimerManager.h

--- C++/Projects/Clock/Clock/AlarmManager.h
+++ C++/Projects/Clock/Clock/AlarmManager.h
@@ -2,10 +2,27 @@
 #define ALARMMANAGER_H
 
 #include <iostream>
+#include <chrono>
+
+class Alarm {
+public:
+	std::chrono::system_clock::time_point time;
+	std::string label; // "School", "Breakfast", "Meeting", etc.
+	bool enabled;
+};
 
 class AlarmManager {
+private:
+	std::vector<Alarm> alarms;
 public:
-	AlarmManager();
+	AlarmManager() = default;
+
+	void addAlarm(const Alarm& alarm);
+	void removeAlarm(int index);
+	void enableAlarm(int index, bool enable);
+	std::vector<Alarm> getActiveAlarms() const;
+
+	void checkAndTriggerAlarms(); // periodically call to run alarms at the set time
 };
 
 #endif
