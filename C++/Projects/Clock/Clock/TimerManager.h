63cff23e79d675a6159db858bcc36f0da601accc
MikitaVitkouski <sovenokrelax@gmail.com>
2025-05-15 23:12:05 +0300

    Add class Timer in TimerManager.h, TimerManager's class signature

--- C++/Projects/Clock/Clock/TimerManager.h
+++ C++/Projects/Clock/Clock/TimerManager.h
@@ -2,10 +2,26 @@
 #define TIMERMANAGER_H
 
 #include <iostream>
+#include <chrono>
+
+class Timer {
+	std::chrono::milliseconds duration;
+	std::chrono::steady_clock::time_point startTime;
+	bool running;
+};
 
 class TimerManager {
+private:
+	Timer timer;
 public:
-	TimerManager();
+	TimerManager() = default;
+
+	void start(std::chrono::milliseconds duration);
+	void pause();
+	void resume();
+	void reset();
+	bool isFinished() const;
+	std::chrono::milliseconds remaining() const;
 };
 
 #endif
