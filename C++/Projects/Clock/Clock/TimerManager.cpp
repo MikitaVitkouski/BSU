c2e03fc956ef16baa669dfe75fcb98d5af86fa05
MikitaVitkouski <sovenokrelax@gmail.com>
2025-05-15 23:18:58 +0300

    Implement TimerManager methods: start(), pause(), reset(), resume(), isFinished(). There are some mistakes to be fixed asap

--- C++/Projects/Clock/Clock/TimerManager.cpp
+++ C++/Projects/Clock/Clock/TimerManager.cpp
@@ -1,1 +1,24 @@
 #include "TimerManager.h"
+
+void TimerManager::start(std::chrono::milliseconds duration) {
+	timer.duration = duration;
+}
+
+void TimerManager::pause() {
+	timer.running = false;
+}
+
+void TimerManager::resume() {
+	timer.running = true;
+}
+
+void TimerManager::reset() {
+	timer.duration = 0;
+	timer.running = false;
+	timer.startTime = 0;
+}
+
+bool TimerManager::isFinished() const {
+	return std::chrono::duration_cast<std::chrono::milliseconds>
+		(std::chrono::steady_clock::now() - timer.startTime);
+}
