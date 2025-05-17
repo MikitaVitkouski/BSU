86c3f297337df79709a92629ff5b17bac7b08117
MikitaVitkouski <sovenokrelax@gmail.com>
2025-05-17 16:06:59 +0300

    Apply white-orange stylesheet to our MainWindow's UI

--- C++/Projects/Clock/QtGUI/ClockGUI/mainwindow.cpp
+++ C++/Projects/Clock/QtGUI/ClockGUI/mainwindow.cpp
@@ -8,7 +8,46 @@ MainWindow::MainWindow(QWidget *parent)
     , timer(new QTimer(this))
 {
     ui->setupUi(this);
+QString styleSheet = R"(
+    QWidget {
+        background-color: #fdfdfd;
+        color: #444444;
+        font-family: Segoe UI, sans-serif;
+        font-size: 16px;
+    }
 
+    QLineEdit {
+        background-color: #2a2a2a;
+        color: #000000;
+        border: 2px solid #ffaa00;
+        border-radius: 10px;
+        padding: 6px;
+    }
+
+    QLabel {
+        color: #ff6600;
+        font-weight: bold;
+        font-size: 50px;
+    }
+
+    QPushButton {
+        background-color: #ffffff;
+        color: #333333;
+        border: 1 px solid #e0e0e0;
+        border-radius: 40px;
+        padding: 10px;
+        min-width: 60px;
+    }
+
+    QPushButton:hover {
+        background-color: #f5f5f5;
+    }
+
+    QPushButton:pressed {
+        background-color: #ff9900;
+    }
+)";
+    this->setStyleSheet(styleSheet);
     // Pages
     connect(ui->btnAlarm, &QPushButton::clicked, this, &MainWindow::onAlarmClicked);
     connect(ui->btnStopwatch, &QPushButton::clicked, this, &MainWindow::onStopwatchClicked);
