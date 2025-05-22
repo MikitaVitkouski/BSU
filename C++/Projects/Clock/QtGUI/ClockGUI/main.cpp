ccc221e7b28558a59313ab1daa56222cd76b15e7
MikitaVitkouski <sovenokrelax@gmail.com>
2025-05-22 22:07:38 +0300

    Try to set logo for the Clock, FractionCalculator applications, but it does not work somehow

--- C++/Projects/Clock/QtGUI/ClockGUI/main.cpp
+++ C++/Projects/Clock/QtGUI/ClockGUI/main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icons/resources/clockapp.png"));
     MainWindow w;
+    w.setWindowIcon(QIcon(":/resources/clockapp.png"));
     w.show();
     return a.exec();
 }
