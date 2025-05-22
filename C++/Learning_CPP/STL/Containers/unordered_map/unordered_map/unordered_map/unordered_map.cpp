a32e7eca78cf1dedf8ba45351aaea8d1ad906520
MikitaVitkouski <sovenokrelax@gmail.com>
2025-05-22 22:07:02 +0300

    Example for unordered_map, when we write birth's dates info from input.txt to unordered_map database and to output.txt

---
+++ C++/Learning_CPP/STL/Containers/unordered_map/unordered_map/unordered_map/unordered_map.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <regex>
+#include <unordered_map>
+#include <algorithm>
+#include <fstream>
+#include <string>
+
+int main() {
+	std::ifstream in("input.txt");
+	std::ofstream out("output.txt");
+	if (!in.is_open()) {
+		std::cerr << "Cannot open file input.txt!";
+		return 1;
+	}
+
+	std::unordered_map<std::string, std::string> database;
+	std::string line;
+	std::regex pattern(R"(([A-Z][a-z]+)\s+([A-Z][a-z]+)\s+(\d{2}-\d{2}-\d{4}))");
+	std::smatch match;
+
+	while (std::getline(in, line)) {
+		if (std::regex_search(line, match, pattern)) {
+			std::string name = match[1].str() + " " + match[2].str();
+			std::string date = match[3].str();
+			database[name] = date;
+		}
+	}
+
+
+	in.close();
+
+	for (const auto& [name,date] : database) {
+		out << name << " " << date << std::endl;
+	}
+
+	return 0;
+}
