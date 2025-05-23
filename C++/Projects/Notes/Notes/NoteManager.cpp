69468068a8de6273bc3a2dfc0f88b464de3161d8
MikitaVitkouski <sovenokrelax@gmail.com>
2025-05-23 18:29:26 +0300

    Add field title to Note, add getters for Note's data, change NoteManager class, implement constructor, NoteManager::addNote, NoteManager::removeNote(), NoteManager::getNotes(), NoteManager::getSize()

--- C++/Projects/Notes/Notes/NoteManager.cpp
+++ C++/Projects/Notes/Notes/NoteManager.cpp
@@ -1,2 +1,24 @@
 #include "NoteManager.h"
 
+NoteManager::NoteManager(std::vector<Note> v) : notes(std::move(v)) { }
+
+void NoteManager::addNote(const Note& note) {
+	notes.emplace_back(std::move(note));
+}
+
+void NoteManager::removeNote(int index) {
+	if (index >= 0 && index < notes.size()) {
+		notes.erase(begin(notes) + index);
+	}
+	else {
+		std::cerr << "Invalid index: " << index << "\n";
+	}
+}
+
+std::vector<Note> NoteManager::getNotes() const {
+	return notes;
+}
+
+int NoteManager::getSize() const {
+	return notes.size();
+}
--- C++/Projects/Notes/Notes/NoteManager.h
+++ C++/Projects/Notes/Notes/NoteManager.h
@@ -2,22 +2,29 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 class Note {
 private:
 	std::string note;
+	std::string title;
 public:
-	Note(std::string text) : note(text) {}
+	Note(std::string text, std::string tit) : note(text), title(tit) {}
 	~Note() = default;
+
+	std::string getNote() const { return note; }
+	std::string getTitle() const { return title; }
 };
 
 class NoteManager {
 private:
-	std::vector<std::string> notes;
+	std::vector<Note> notes;
 public:
-	NoteManager(std::vector<std::string> v);
+	NoteManager(std::vector<Note> v);
 	~NoteManager() = default;
-	void addNote(std::string note);
-	void removeNote(std::string note);
-	std::vector<std::string> getNotes();
+	void addNote(const Note& note);
+	void removeNote(int index);
+	std::vector<Note> getNotes() const;
+	int getSize() const;
 };
